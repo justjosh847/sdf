aaf8bd0d4b891329217112429379b7c43fd0fbbd
justjosh847 <nikita.osd1@yandex.ru>
2022-11-20 19:26:08 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from application.application.db import get_employees
+from application.salary import calculate_salary
+from datetime import datetime
+if __name__ == '__main__':
+    print(datetime.now())
+    get_employees()
+    calculate_salary()
